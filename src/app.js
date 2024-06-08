29e0728a75f89522023b7a3c310f03867defd481
Fahim Abdullah <developerfahim134@gmail.com>
2024-06-09 01:03:48 +0530

    forgot to update app.js and asynHaanlder.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,20 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors(
-  origin: process.env.CORS_ORIGIN
-));
+// for middlewares use always app.use , and here the cors is a middlware
+
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials:true
+}));
+
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 
 export { app }
