6b9c9a0c6c0fe78b9ceec2a8b433c69e9960d3bd
Fahim Abdullah <developerfahim134@gmail.com>
2024-06-09 01:01:25 +0530

    ultils are added and disccussed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-
 import dotenv from 'dotenv'
 
-
-
 import connectDB from "./db/index.js";
-
-
 
 dotenv.config({
   path: './env'
 })
 
 
-
 connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 7000, () => {
+    console.log(`server is running at port : ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.error("mondo db connection failed !!!",err)
+})
---
+++ src/utils/apiError.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message="someting went rong",
+    error = [],
+    stack = ""
+  ){
+    super(message)
+    this.statusCode = statusCode
+    this.data = null
+    this.message = message
+    this.success = success
+    this.error = error 
+
+    if (stack) {
+      this.stack = stack
+      
+    }else{
+      Error.captureStackTrace(this,this.constructor)
+    }
+
+  }
+}
+
+
+export { ApiError } 
